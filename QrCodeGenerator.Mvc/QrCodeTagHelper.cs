d1476bff3608970274bc76e1a62d3d3df5b6791c
Mateus Bernardi Marcato <mateus02_bernardi@hotmail.com>
2025-08-27 14:10:02 -0300

    Fixes

--- QrCodeGenerator.Mvc/QrCodeTagHelper.cs
+++ QrCodeGenerator.Mvc/QrCodeTagHelper.cs
@@ -6,7 +6,9 @@ using System.IO;
 
 namespace QrCodeGenerator.Mvc;
 
+[HtmlTargetElement("qrcode", Attributes = "data", TagStructure = TagStructure.WithoutEndTag)]
 [HtmlTargetElement("qrcode", Attributes = "data,border", TagStructure = TagStructure.WithoutEndTag)]
+[HtmlTargetElement("qrcode", Attributes = "data,scale", TagStructure = TagStructure.WithoutEndTag)]
 [HtmlTargetElement("qrcode", Attributes = "data,scale,border", TagStructure = TagStructure.WithoutEndTag)]
 public class QrCodeTagHelper : TagHelper
 {
@@ -28,27 +30,25 @@ public class QrCodeTagHelper : TagHelper
 
     public override void Process(TagHelperContext context, TagHelperOutput output)
     {
-        if (string.IsNullOrWhiteSpace(Data) || Border < 0)
+        if (string.IsNullOrWhiteSpace(Data))
         {
             output.SuppressOutput();
             return;
         }
 
+        var border = Math.Max(Border, 0);
+
         var qrCode = QrCode.EncodeText(Data, _configuration.ErrorCorrectionLevel);
         if (_configuration.Format == QrCodeFormat.Svg)
         {
-            var svg = qrCode.ToSvgString(Border);
+            var svg = qrCode.ToSvgString(border);
             output.Content.AppendHtml(svg);
         }
         else
         {
-            if (Scale <= 0)
-            {
-                output.SuppressOutput();
-                return;
-            }
+            var scale = Math.Max(Scale, 1);
 
-            var bitmap = qrCode.ToImage(Scale, Border);
+            var bitmap = qrCode.ToImage(scale, border);
             using var ms = new MemoryStream();
             bitmap.Save(ms, new PngEncoder());
 
